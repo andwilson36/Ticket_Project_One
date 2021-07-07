725739846e8e1fb6a262c268cde46c828d1c25e8
Andrew Wilson <Wilsonandrew57@gmail.com>
2021-07-07 10:49:04 -0500

    hotel.html has smooth scroll and buttons work

--- assets/js/jscriptHotel.js
+++ assets/js/jscriptHotel.js
@@ -17,3 +17,32 @@
 $('.flights-btn').on('click', function() {
     window.location.assign('./index.html');
 });
+
+// Book listeners
+$('.la-text').on('click', function(event) {
+    event.preventDefault();
+
+    $(document).scrollTop(1);
+    $(".arrivingAt").val('Los Angeles');
+});
+
+$('.hou-text').on('click', function(event) {
+    event.preventDefault();
+
+    $(document).scrollTop(1);
+    $(".arrivingAt").val('Houston');
+});
+
+$('.chicago-text').on('click', function(event) {
+    event.preventDefault();
+
+    $(document).scrollTop(1);
+    $(".arrivingAt").val('Chicago');
+});
+
+$('.miami-text').on('click', function(event) {
+    event.preventDefault();
+
+    $(document).scrollTop(1);
+    $(".arrivingAt").val('Miami');
+});
